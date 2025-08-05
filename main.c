13033e1448b967da052f1bbf386271261e06a8b8
Secchinho <raphasec@gmail.com>
2025-08-05 00:12:30 -0300

    leitura de dados completa

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main(int argc, char *argv[])
     int cidade1 = 0;
     int cidade2 = 0;
     int tamEstrada = 0;
+    int origem = 0;
+    int destino = 0;
+    int qtdProibidas = 0;
 
     if(argc != 2)
     {
@@ -32,7 +35,13 @@ int main(int argc, char *argv[])
         fscanf(arq, "%d", &cidade2);
         fscanf(arq, "%d", &tamEstrada);
     }
-    printf("\n");
+
+    printf("Digite a cidade de origem de YL: ");
+    scanf(" %d", &origem);
+    printf("Digite a cidade de destino de YL: ");
+    scanf(" %d", &destino);
+    printf("Digite a quantidade de cidades proibidas para YL: ");
+    scanf(" %d", &qtdProibidas);
 
     fclose(arq);
     return 0;
